8d570a12aecea48176bd395406c69fe2702d3f4c
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-07-13 22:18:07 +0200

    Experiments

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,34 @@
-const x = 5;
+import * as Effect from "@effect/io/Effect";
+import * as Logger from "@effect/io/Logger";
+import * as LoggerLevel from "@effect/io/Logger/Level";
 
-console.log(x);
+//  Effect program
+
+const program = Effect.sync(() => "Hello, world!");
+
+const result = Effect.runSync(program);
+
+console.log(result);
+
+// Pipes
+
+const addSuffix = (input: string) => Effect.succeed(`${input}-example-suffix`);
+
+const examplePipe = Effect.succeed("test" as const).pipe(
+	Effect.flatMap(value => addSuffix(value)),
+	Effect.tap(Effect.log),
+	Effect.tap(() => Effect.sleep("5 seconds")),
+	Effect.flatMap(value =>
+		Effect.if(value === "text-example-suffix", {
+			onFalse: Effect.succeed("correct"),
+			onTrue: Effect.succeed("invalid"),
+		}),
+	),
+	Effect.flatMap(value => addSuffix(value)),
+	Effect.tap(value => Effect.log(value, { level: "Trace" })),
+	Logger.withMinimumLogLevel(LoggerLevel.fromLiteral("All")),
+);
+
+const pipeResult = await Effect.runPromise(examplePipe);
+
+console.log(pipeResult);
