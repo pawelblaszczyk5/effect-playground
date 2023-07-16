fb8b8103df107b60dc2daf428aeeed552dd366a1
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-07-16 23:01:06 +0200

    Remove placeholder

--- src/service.ts
+++
@@ -1,9 +0,0 @@
-import { Context, Effect } from "effect";
-
-type Random = {
-	readonly next: () => Effect.Effect<never, never, number>;
-};
-
-const Random = Context.Tag<Random>();
-
-Random.pipe(Effect.flatMap(t => t.next()));
