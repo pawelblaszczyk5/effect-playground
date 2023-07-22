872893dc390416d2343ec6d822b064cd218c681b
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-07-22 22:53:12 +0200

    Make program serial

--- src/main.ts
+++ src/main.ts
@@ -11,37 +11,34 @@ const getUserName = (id: string) =>
 	);
 
 await Effect.runPromise(
-	Effect.all(
-		[
-			getUserName("test123").pipe(
-				Effect.provideService(UserService, UserServiceLive),
-				Effect.matchEffect({
-					onFailure: ({ _tag }) => Effect.log(`Fetching username errored with a tag ${_tag}`),
-					onSuccess: username => Effect.log(`Fetching username succeeded - ${username}`),
-				}),
-			),
-			getUserName("5678").pipe(
-				Effect.provideService(UserService, UserServiceLive),
-				Effect.matchEffect({
-					onFailure: ({ _tag }) => Effect.log(`Fetching username errored with a tag ${_tag}`),
-					onSuccess: username => Effect.log(`Fetching username succeeded - ${username}`),
-				}),
-			),
-			getUserName("UUID81238123").pipe(
-				Effect.provideService(UserService, {
-					getUser: () =>
-						Effect.succeed({ id: "SOME_MOCK_DATA", username: "Jon Test Snow" }).pipe(
-							Effect.tap(() => Effect.sleep("6 seconds")),
-						),
-				}),
-				Effect.matchEffect({
-					onFailure: ({ _tag }) => Effect.log(`Fetching username errored with a tag ${_tag}`),
-					onSuccess: username => Effect.log(`Fetching username succeeded - ${username}`),
-				}),
-			),
-		],
-		{ concurrency: 2 },
-	),
+	Effect.all([
+		getUserName("test123").pipe(
+			Effect.provideService(UserService, UserServiceLive),
+			Effect.matchEffect({
+				onFailure: ({ _tag }) => Effect.log(`Fetching username errored with a tag ${_tag}`),
+				onSuccess: username => Effect.log(`Fetching username succeeded - ${username}`),
+			}),
+		),
+		getUserName("5678").pipe(
+			Effect.provideService(UserService, UserServiceLive),
+			Effect.matchEffect({
+				onFailure: ({ _tag }) => Effect.log(`Fetching username errored with a tag ${_tag}`),
+				onSuccess: username => Effect.log(`Fetching username succeeded - ${username}`),
+			}),
+		),
+		getUserName("UUID81238123").pipe(
+			Effect.provideService(UserService, {
+				getUser: () =>
+					Effect.succeed({ id: "SOME_MOCK_DATA", username: "Jon Test Snow" }).pipe(
+						Effect.tap(() => Effect.sleep("6 seconds")),
+					),
+			}),
+			Effect.matchEffect({
+				onFailure: ({ _tag }) => Effect.log(`Fetching username errored with a tag ${_tag}`),
+				onSuccess: username => Effect.log(`Fetching username succeeded - ${username}`),
+			}),
+		),
+	]),
 );
 
 Effect.log("Finished").pipe(Effect.runSync);
