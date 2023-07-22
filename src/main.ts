07b0c49b7d5b88f7098fb68f28605c7ef5f351c3
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-07-22 22:39:08 +0200

    Some random experiments

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,22 @@
-import { Context, Effect, Logger, LoggerLevel } from "effect";
+import { Context, Effect } from "effect";
 
 import * as UserServiceLive from "~/user";
 
-//  Effect program
-
-const program = Effect.sync(() => "Hello, world!");
-
-Effect.runSync(program.pipe(Effect.tap(Effect.log)));
-
-// Pipes
-
-const now = Effect.sync(() => Date.now());
-
-const elapsed = <R, E, A>(self: Effect.Effect<R, E, A>): Effect.Effect<R, E, A> =>
-	Effect.Do.pipe(
-		Effect.bind("startMilliseconds", () => now),
-		Effect.bind("result", () => self),
-		Effect.bind("endMilliseconds", () => now),
-		Effect.tap(({ endMilliseconds, startMilliseconds }) =>
-			Effect.log(`Elapsed: ${endMilliseconds - startMilliseconds}`),
-		),
-		Effect.map(({ result }) => result),
-	);
-
-const addSuffix = (input: string) => Effect.succeed(`${input}-example-suffix`);
-
-const examplePipe = (self: Effect.Effect<never, never, string>) =>
-	self.pipe(
-		Effect.flatMap(value => addSuffix(value)),
-		Effect.tap(Effect.log),
-		Effect.tap(() => Effect.sleep("5 seconds")),
-		Effect.flatMap(value =>
-			Effect.if(value === "test-example-suffix", {
-				onFalse: Effect.succeed("invalid"),
-				onTrue: Effect.succeed("correct"),
-			}),
-		),
-		Effect.tap(value => Effect.log(value, "Trace")),
-		Logger.withMinimumLogLevel(LoggerLevel.fromLiteral("All")),
-		elapsed,
-	);
-
-void Effect.runPromise(Effect.succeed("test").pipe(examplePipe));
-void Effect.runPromise(Effect.succeed("test2").pipe(examplePipe));
-
 const UserService = Context.Tag<typeof import("~/user")>();
 
-const programWithService = UserService.pipe(
-	Effect.flatMap(UserService => Effect.succeed(UserService.getUser())),
-	Effect.tap(user => Effect.log(JSON.stringify(user))),
+const getUserNameProgram = Effect.Do.pipe(
+	Effect.bind("userService", () => UserService),
+	Effect.flatMap(({ userService }) => userService.getUser("test123").pipe(Effect.map(({ username }) => username))),
 );
 
-const runnableProgramWithService = Effect.provideService(programWithService, UserService, UserServiceLive);
+const contextWithUserService = Context.empty().pipe(Context.add(UserService, UserServiceLive));
 
-Effect.runSync(runnableProgramWithService);
+await Effect.provideContext(getUserNameProgram, contextWithUserService).pipe(
+	Effect.matchEffect({
+		onFailure: ({ _tag }) => Effect.log(`Fetching username errored with a tag ${_tag}`),
+		onSuccess: username => Effect.log(`Fetching username succeeded - ${username}`),
+	}),
+	Effect.runPromise,
+);
+
+Effect.log("Finished").pipe(Effect.runSync);
--- src/user.ts
+++ src/user.ts
@@ -1,5 +1,25 @@
-export const getUser = () => ({
-	id: "example-id",
-	name: "John",
-	surname: "Mark",
-});
+import { Effect } from "effect";
+
+const createTaggedErrorBuilder =
+	<TTag extends string>(tag: TTag) =>
+	() =>
+		({
+			_tag: tag,
+		}) as const;
+
+const createMissingUserError = createTaggedErrorBuilder("MISSING_USER");
+
+export const getUser = (id: string) =>
+	Effect.succeed(id).pipe(
+		Effect.flatMap(value =>
+			Effect.if(value === "test123", {
+				onFalse: Effect.fail(createMissingUserError()),
+				onTrue: Effect.succeed({
+					id: "test123",
+					username: "Jon Snow",
+				}),
+			}),
+		),
+		Effect.tap(() => Effect.sleep("2 seconds")),
+		Effect.tap(user => Effect.log(`Successfully fetched user with id ${user.id}`)),
+	);
