a3a7ed71f24eeb27db633730d4cc14c147a82070
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-07-22 22:52:39 +0200

    Parametrize getUserName

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,30 @@ import * as UserServiceLive from "~/user";
 
 const UserService = Context.Tag<typeof import("~/user")>();
 
-const getUserNameProgram = Effect.Do.pipe(
-	Effect.bind("userService", () => UserService),
-	Effect.flatMap(({ userService }) => userService.getUser("test123").pipe(Effect.map(({ username }) => username))),
-);
+const getUserName = (id: string) =>
+	Effect.Do.pipe(
+		Effect.bind("userService", () => UserService),
+		Effect.flatMap(({ userService }) => userService.getUser(id).pipe(Effect.map(({ username }) => username))),
+	);
 
 await Effect.runPromise(
 	Effect.all(
 		[
-			getUserNameProgram.pipe(
+			getUserName("test123").pipe(
 				Effect.provideService(UserService, UserServiceLive),
 				Effect.matchEffect({
 					onFailure: ({ _tag }) => Effect.log(`Fetching username errored with a tag ${_tag}`),
 					onSuccess: username => Effect.log(`Fetching username succeeded - ${username}`),
 				}),
 			),
-			getUserNameProgram.pipe(
+			getUserName("5678").pipe(
+				Effect.provideService(UserService, UserServiceLive),
+				Effect.matchEffect({
+					onFailure: ({ _tag }) => Effect.log(`Fetching username errored with a tag ${_tag}`),
+					onSuccess: username => Effect.log(`Fetching username succeeded - ${username}`),
+				}),
+			),
+			getUserName("UUID81238123").pipe(
 				Effect.provideService(UserService, {
 					getUser: () =>
 						Effect.succeed({ id: "SOME_MOCK_DATA", username: "Jon Test Snow" }).pipe(
