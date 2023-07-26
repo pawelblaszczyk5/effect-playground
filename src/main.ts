3aab9f00aaec64f19200f8a26856fcd687695f96
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-07-26 20:34:14 +0200

    Experiment more

--- src/main.ts
+++ src/main.ts
@@ -58,6 +58,6 @@ declare const authState:
 	| { rights: Array<string>; status: "admin" };
 
 const someState = Match.value(authState).pipe(
-	Match.discriminators("status")({ admin: ({ status }) => status }),
+	Match.discriminator("status")("admin", ({ status }) => status),
 	Match.orElse(() => "not logged in as admin" as const),
 );
